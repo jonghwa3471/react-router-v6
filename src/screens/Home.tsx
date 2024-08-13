d08b4e4b200ec839945a5c57ce65dbaed5396556
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-08-13 20:27:49 +0900

    #4.8 Extras

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+  console.log(readSearchParams.get("geo"));
+  setTimeout(() => {
+    setSearchParams({
+      day: "today",
+      tomorrow: "Tue",
+    });
+  }, 3000);
   return (
     <div>
       <h1>Users</h1>
